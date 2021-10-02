321229cf594fb06c3e6fbd8bbc3f6ae26f48989b
tonystore <thony918@outlook.com>
2021-10-02 16:02:47 -0500

    Primer Comit

---
+++ database/seeds/PermissioSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\PermissionRegistrar;
+
+class PermissioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        app()[PermissionRegistrar::class]->forgetCachedPermissions();
+        $role = Role::create(['name' => 'super-admin', 'description' => 'Super Administrador']);
+        DB::table('users')->insert([
+            'username'        => 'administrador',
+            'names'         => 'Administrador',
+            'email'           => 'admin@cork-admin.com',
+            'password'        => Hash::make('12345678'),
+            'status'          => 'activo',
+            'created_at'      => now(),
+            'updated_at'      => now()
+        ]);
+        $user = User::find(1);
+        $user->assignRole($role);
+    }
+}
